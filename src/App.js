a347e5272adaa02d7a7bd88d324ceeeb85a4ca0c
Kapil sharma <kapil@targetbay.com>
2020-02-11 18:10:33 +0530

    added the route changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/HomePage.component'
 
+const HatsPage = props => {
+console.log(props); 
+  return ( <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)};
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/>
     </div>
   );
 }
