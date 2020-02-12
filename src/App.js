f49209f76165ea9e5dc96e16326a8cc6de642961
Kapil sharma <kapil@targetbay.com>
2020-02-12 14:09:38 +0530

    added the shop page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/HomePage.component'
-
-const HatsPage = props => {
-console.log(props); 
-  return ( <div>
-    <h1> HATS PAGE </h1>
-  </div>
-)};
+import Shop from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={Shop}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import SHOP_DATA from './shop_data'
+import './shop.scss'
+
+class Shop extends React.Component 
+{
+    constructor(props) {
+        super();
+        this.state = {
+            collections : SHOP_DATA
+        }
+    }
+
+    render() {
+        const {collections} = this.state;
+        return (
+            <div>
+                Shop
+                {
+                    collections
+                    .map(({id, ...otherCollectionProps}) => (
+                        <CollectionPreview key={id} {...otherCollectionProps}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Shop
